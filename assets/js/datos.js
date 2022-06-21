a51c70b4c87fe95a47371714cfb01c30bf5728d5
Macarena Garcia <garciamaca_07@hotmail.com>
2022-06-20 22:02:07 -0300

    recalculando JS

---
+++ assets/js/datos.js
@@ -0,0 +1,255 @@
+const BBDD = [
+    {
+        "id": 1,
+        "nombre": "Burguer Clasic 1",
+        "img": "/assets/img/menu/burger4.jpg",
+        "precio": 750,
+        "cantidad": 1
+    },
+    {
+        "id": 2,
+        "nombre": "Burguer Clasic 2 ",
+        "img": "/assets/img/menu/burguefritas.jpg",
+        "precio": 1200,
+        "cantidad": 1
+    },
+    {
+        "id": 3,
+        "nombre": "Burguer Alone",
+        "img": "/assets/img/menu/burguer.jpg",
+        "precio": 450,
+        "cantidad": 1
+    },
+    {
+        "id": 4,
+        "nombre": "Burguer Alone 2",
+        "img": "/assets/img/menu/burguer2.jpg",
+        "precio": 560,
+        "cantidad": 1
+    },
+    {
+        "id": 5,
+        "nombre": "Burguer Special",
+        "img": "/assets/img/menu/burguer5.jpg",
+        "precio": 1200,
+        "cantidad": 1
+    },
+    {
+        "id": 6,
+        "nombre": "Pizza Capresse",
+        "img": "/assets/img/menu/pizza.jpg",
+        "precio": 1500,
+        "cantidad": 1
+    },
+    {
+        "id": 7,
+        "nombre": "Pizza muzarella y morron",
+        "img": "/assets/img/menu/pizza2.jpg",
+        "precio": 1650,
+        "cantidad": 1
+    },
+    {
+        "id": 8,
+        "nombre": "Pizza napolitana individual",
+        "img": "/assets/img/menu/pizza3.jpg",
+        "precio": 800,
+        "cantidad": 1
+    },
+    {
+        "id": 9,
+        "nombre": "Ensalada opcion 1",
+        "img": "/assets/img/menu/salad.jpg",
+        "precio": 600,
+        "cantidad": 1
+    },
+    {
+        "id": 10,
+        "nombre": "Ensalada completa",
+        "img": "/assets/img/menu/salad2.jpg",
+        "precio": 1200,
+        "cantidad": 1
+    },
+    {
+        "id": 11,
+        "nombre": "Ensalada opcion 2",
+        "img": "/assets/img/menu/salad3.jpg",
+        "precio": 1100,
+        "cantidad": 1
+    },
+    {
+        "id": 12,
+        "nombre": "Ensalada completa 2",
+        "img": "/assets/img/menu/salad4.jpg",
+        "precio": 1500,
+        "cantidad": 1
+    },
+    {
+        "id": 13,
+        "nombre": "Pulpo Grillado",
+        "img": "/assets/img/menu/seafood.jpg",
+        "precio": 1800,
+        "cantidad": 1
+    },
+    {
+        "id": 14,
+        "nombre": "Cazuelsa de mariscos",
+        "img": "/assets/img/menu/seafood1.jpg",
+        "precio": 2800,
+        "cantidad": 1
+    },
+    {
+        "id": 15,
+        "nombre": "Tabla surtida sushi",
+        "img": "/assets/img/menu/sushi.jpg",
+        "precio": 3500,
+        "cantidad": 1
+    },
+    {
+        "id": 16,
+        "nombre": "Pasta fetuchine con salsa capresse",
+        "img": "/assets/img/menu/pasta1.jpg",
+        "precio": 1500,
+        "cantidad": 1
+    },
+    {
+        "id": 17,
+        "nombre": "Pasta en ensalada",
+        "img": "/assets/img/menu/pasta-1854245__340.jpg",
+        "precio": 1200,
+        "cantidad": 1
+    },
+    {
+        "id": 18,
+        "nombre": "Pasta gratinada",
+        "img": "/assets/img/menu/pastagratinada.jpg",
+        "precio": 1500,
+        "cantidad": 1
+    },
+    {
+        "id": 19,
+        "nombre": "Pasta con carne de ternera",
+        "img": "/assets/img/menu/pasta.jpg",
+        "precio": 1600,
+        "cantidad": 1
+    },
+    {
+        "id": 20,
+        "nombre": "Licuados frutales variados",
+        "img": "/assets/img/menu/licuados.jpg",
+        "precio": 800,
+        "cantidad": 1
+    },
+    {
+        "id": 21,
+        "nombre": "Limonada",
+        "img": "/assets/img/menu/limonada.jpg",
+        "precio": 800,
+        "cantidad": 1
+    },
+    {
+        "id": 22,
+        "nombre": "Porron de cerveza a eleccion",
+        "img": "/assets/img/menu/beers2.jpg",
+        "precio": 850,
+        "cantidad": 1
+    },
+    {
+        "id": 23,
+        "nombre": "Pruebe nuestra carta de vinos",
+        "img": "/assets/img/menu/wines.jpg",
+        "precio": 1200,
+        "cantidad": 1
+    },
+    {
+        "id": 24,
+        "nombre": "Daiquiri frutilla",
+        "img": "/assets/img/menu/trago7.jpg",
+        "precio": 1500,
+        "cantidad": 1
+    },
+    {
+        "id": 25,
+        "nombre": "Naranja y lima",
+        "img": "/assets/img/menu/trago5.jpg",
+        "precio": 1600,
+        "cantidad": 1
+    },
+    {
+        "id": 26,
+        "nombre": "Kiwi y kale",
+        "img": "/assets/img/menu/trago1.jpg",
+        "precio": 1200,
+        "cantidad": 1
+    },
+    {
+        "id": 27,
+        "nombre": "Desayuno/merienda",
+        "img": "/assets/img/menu/snack1.jpg",
+        "precio": 2800,
+        "cantidad": 1
+    },
+    {
+        "id": 28,
+        "nombre": "Desayuno/merienda 2",
+        "img": "/assets/img/menu/snack2.jpg",
+        "precio": 2600,
+        "cantidad": 1
+    },
+    {
+        "id": 29,
+        "nombre": "Porcion torta 1",
+        "img": "/assets/img/menu/cake2.jpg",
+        "precio": 1300,
+        "cantidad": 1
+    },
+    {
+        "id": 30,
+        "nombre": "Porcion de torta 2",
+        "img": "/assets/img/menu/cake3.jpg",
+        "precio": 1500,
+        "cantidad": 1
+    },
+    {
+        "id": 31,
+        "nombre": "Dunnats",
+        "img": "/assets/img/menu/donas.jpg",
+        "precio": 350,
+        "cantidad": 1
+    },
+    {
+        "id": 32,
+        "nombre": "Croissants",
+        "img": "/assets/img/menu/croasants.jpg",
+        "precio": 200,
+        "cantidad": 1
+    },
+    {
+        "id": 33,
+        "nombre": "Facturas variadas a eleccion",
+        "img": "/assets/img/menu/facturas2.jpg",
+        "precio": 2500,
+        "cantidad": 1
+    },
+    {
+        "id": 34,
+        "nombre": "Macaroons a eleccion",
+        "img": "/assets/img/menu/macaroons1.jpg",
+        "precio": 500,
+        "cantidad": 1
+    },
+    {
+        "id": 35,
+        "nombre": "Pancake a eleccion",
+        "img": "/assets/img/menu/pancakes.jpg",
+        "precio": 1500,
+        "cantidad": 1
+    },
+    {
+        "id": 36,
+        "nombre": "Cafes y tes a eleccion",
+        "img": "/assets/img/menu/coffee2.jpg",
+        "precio": 1000,
+        "cantidad": 1
+    },
+
+]
--- assets/js/delivery.js
+++ assets/js/delivery.js
@@ -1,259 +1,28 @@
-const BBDD = [
-    {
-        "id": 1,
-        "nombre": "Burguer Clasic 1",
-        "img": "/assets/img/menu/burger4.jpg",
-        "precio": 750,
-        "cantidad": 1
-    },
-    {
-        "id": 2,
-        "nombre": "Burguer Clasic 2 ",
-        "img": "/assets/img/menu/burguefritas.jpg",
-        "precio": 1200,
-        "cantidad": 1
-    },
-    {
-        "id": 3,
-        "nombre": "Burguer Alone",
-        "img": "/assets/img/menu/burguer.jpg",
-        "precio": 450,
-        "cantidad": 1
-    },
-    {
-        "id": 4,
-        "nombre": "Burguer Alone 2",
-        "img": "/assets/img/menu/burguer2.jpg",
-        "precio": 560,
-        "cantidad": 1
-    },
-    {
-        "id": 5,
-        "nombre": "Burguer Special",
-        "img": "/assets/img/menu/burguer5.jpg",
-        "precio": 1200,
-        "cantidad": 1
-    },
-    {
-        "id": 6,
-        "nombre": "Pizza Capresse",
-        "img": "/assets/img/menu/pizza.jpg",
-        "precio": 1500,
-        "cantidad": 1
-    },
-    {
-        "id": 7,
-        "nombre": "Pizza muzarella y morron",
-        "img": "/assets/img/menu/pizza2.jpg",
-        "precio": 1650,
-        "cantidad": 1
-    },
-    {
-        "id": 8,
-        "nombre": "Pizza napolitana individual",
-        "img": "/assets/img/menu/pizza3.jpg",
-        "precio": 800,
-        "cantidad": 1
-    },
-    {
-        "id": 9,
-        "nombre": "Ensalada opcion 1",
-        "img": "/assets/img/menu/salad.jpg",
-        "precio": 600,
-        "cantidad": 1
-    },
-    {
-        "id": 10,
-        "nombre": "Ensalada completa",
-        "img": "/assets/img/menu/salad2.jpg",
-        "precio": 1200,
-        "cantidad": 1
-    },
-    {
-        "id": 11,
-        "nombre": "Ensalada opcion 2",
-        "img": "/assets/img/menu/salad3.jpg",
-        "precio": 1100,
-        "cantidad": 1
-    },
-    {
-        "id": 12,
-        "nombre": "Ensalada completa 2",
-        "img": "/assets/img/menu/salad4.jpg",
-        "precio": 1500,
-        "cantidad": 1
-    },
-    {
-        "id": 13,
-        "nombre": "Pulpo Grillado",
-        "img": "/assets/img/menu/seafood.jpg",
-        "precio": 1800,
-        "cantidad": 1
-    },
-    {
-        "id": 14,
-        "nombre": "Cazuelsa de mariscos",
-        "img": "/assets/img/menu/seafood1.jpg",
-        "precio": 2800,
-        "cantidad": 1
-    },
-    {
-        "id": 15,
-        "nombre": "Tabla surtida sushi",
-        "img": "/assets/img/menu/sushi.jpg",
-        "precio": 3500,
-        "cantidad": 1
-    },
-    {
-        "id": 16,
-        "nombre": "Pasta fetuchine con salsa capresse",
-        "img": "/assets/img/menu/pasta1.jpg",
-        "precio": 1500,
-        "cantidad": 1
-    },
-    {
-        "id": 17,
-        "nombre": "Pasta en ensalada",
-        "img": "/assets/img/menu/pasta-1854245__340.jpg",
-        "precio": 1200,
-        "cantidad": 1
-    },
-    {
-        "id": 18,
-        "nombre": "Pasta gratinada",
-        "img": "/assets/img/menu/pastagratinada.jpg",
-        "precio": 1500,
-        "cantidad": 1
-    },
-    {
-        "id": 19,
-        "nombre": "Pasta con carne de ternera",
-        "img": "/assets/img/menu/pasta.jpg",
-        "precio": 1600,
-        "cantidad": 1
-    },
-    {
-        "id": 20,
-        "nombre": "Licuados frutales variados",
-        "img": "/assets/img/menu/licuados.jpg",
-        "precio": 800,
-        "cantidad": 1
-    },
-    {
-        "id": 21,
-        "nombre": "Limonada",
-        "img": "/assets/img/menu/limonada.jpg",
-        "precio": 800,
-        "cantidad": 1
-    },
-    {
-        "id": 22,
-        "nombre": "Porron de cerveza a eleccion",
-        "img": "/assets/img/menu/beers2.jpg",
-        "precio": 850,
-        "cantidad": 1
-    },
-    {
-        "id": 23,
-        "nombre": "Pruebe nuestra carta de vinos",
-        "img": "/assets/img/menu/wines.jpg",
-        "precio": 1200,
-        "cantidad": 1
-    },
-    {
-        "id": 24,
-        "nombre": "Daiquiri frutilla",
-        "img": "/assets/img/menu/trago7.jpg",
-        "precio": 1500,
-        "cantidad": 1
-    },
-    {
-        "id": 25,
-        "nombre": "Naranja y lima",
-        "img": "/assets/img/menu/trago5.jpg",
-        "precio": 1600,
-        "cantidad": 1
-    },
-    {
-        "id": 26,
-        "nombre": "Kiwi y kale",
-        "img": "/assets/img/menu/trago1.jpg",
-        "precio": 1200,
-        "cantidad": 1
-    },
-    {
-        "id": 27,
-        "nombre": "Desayuno/merienda",
-        "img": "/assets/img/menu/snack1.jpg",
-        "precio": 2800,
-        "cantidad": 1
-    },
-    {
-        "id": 28,
-        "nombre": "Desayuno/merienda 2",
-        "img": "/assets/img/menu/snack2.jpg",
-        "precio": 2600,
-        "cantidad": 1
-    },
-    {
-        "id": 29,
-        "nombre": "Porcion torta 1",
-        "img": "/assets/img/menu/cake2.jpg",
-        "precio": 1300,
-        "cantidad": 1
-    },
-    {
-        "id": 30,
-        "nombre": "Porcion de torta 2",
-        "img": "/assets/img/menu/cake3.jpg",
-        "precio": 1500,
-        "cantidad": 1
-    },
-    {
-        "id": 31,
-        "nombre": "Dunnats",
-        "img": "/assets/img/menu/donas.jpg",
-        "precio": 350,
-        "cantidad": 1
-    },
-    {
-        "id": 32,
-        "nombre": "Croissants",
-        "img": "/assets/img/menu/croasants.jpg",
-        "precio": 200,
-        "cantidad": 1
-    },
-    {
-        "id": 33,
-        "nombre": "Facturas variadas a eleccion",
-        "img": "/assets/img/menu/facturas2.jpg",
-        "precio": 2500,
-        "cantidad": 1
-    },
-    {
-        "id": 34,
-        "nombre": "Macaroons a eleccion",
-        "img": "/assets/img/menu/macaroons1.jpg",
-        "precio": 500,
-        "cantidad": 1
-    },
-    {
-        "id": 35,
-        "nombre": "Pancake a eleccion",
-        "img": "/assets/img/menu/pancakes.jpg",
-        "precio": 1500,
-        "cantidad": 1
-    },
-    {
-        "id": 36,
-        "nombre": "Cafes y tes a eleccion",
-        "img": "/assets/img/menu/coffee2.jpg",
-        "precio": 1000,
-        "cantidad": 1
-    },
 
-]
 
+function Cliente(nombre, telefono, direccion) {
+    this.nombre = nombre;
+    this.telefono = telefono;
+    this.direccion = direccion;
+}
+
+  function e (id, nombre, precio, imagen) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.imagen = imagen;
+  }
+  
+
+  function Pedido() {
+    this.cliente = undefined;
+    this.items = [];
+    this.total = 0;
+    var fecha = new Date();
+    fecha = fecha.getDate() + "/" + fecha.getMonth() + "/" + fecha.getFullYear();
+    this.fecha = fecha;
+  }
+  
 const carrito = [];
 
 let total = 0;
@@ -262,24 +31,25 @@ function renderizarProductos() {
 
     let tienda = document.getElementById('tienda');
     let filtro = document.getElementById('filtro');
+    
     filtro.innerHTML = `
      <button class="btn btn-warning mb-5" onclick="filtroprecio()">Filtrar Precio mayor a $500</button>` 
 
     BBDD.forEach((e) => {
 
       let productoHTML = 
-      `
-        <img src="${e.img}" alt=BURGER" style="width:100%">
+      ` <img src="${e.img}" alt=BURGER" style="width:100%">
         <h1>${e.nombre}</h1>
         <p class="price">${e.precio}$</p>
         <p>Hamburguesa vegetariana de garbanzos con cebolla tomate y lechuga con guarnicion de calabazas fritas</p>
         <p><button onclick="agregarProductoAlCarrito(${e.id})">Add to Cart</button></p>
-           
+        <p><button onclick="eliminarProductoDelCarrito(${e.id})">Quit to Cart</button></p>
         `
+
         tienda.innerHTML += productoHTML 
     });
 
-renderizarProductos();
+    renderizarProductos();
 
 } 
 
@@ -287,7 +57,7 @@ function agregarProductoAlCarrito(id){
 
     let producto = BBDD.find(producto => producto.id == id);
   
-   let productoEnCarrito = carrito.find(producto => producto.id == id);
+    let productoEnCarrito = carrito.find(producto => producto.id == id);
     
     if(productoEnCarrito){
         
@@ -299,7 +69,8 @@ function agregarProductoAlCarrito(id){
         carrito.push(producto);
              
     }
-   renderizarCarrito(); 
+    
+    renderizarCarrito(); 
 }
 
 function renderizarCarrito(){
@@ -315,6 +86,7 @@ function renderizarCarrito(){
        <img src="${producto.img}" alt=BURGER" style="width:100%">
        <h1>${producto.nombre}</h1>
        <p class="price">${producto.precio}$</p>
+       <p><button onClick="agregarProductoDelCarrito${producto.cantidad}">Add to Cart</button>
        <p><button onClick="eliminarProductoDelCarrito${producto.cantidad}">Quit to Cart</button>
        `
       })
@@ -322,34 +94,34 @@ function renderizarCarrito(){
     carritoHTML.innerHTML = html;
 
     calcularTotal(); 
-
 }
 
 function calcularTotal(){
 
     carrito.forEach((producto) => {
-        
-     
-        total = producto.precio * producto.cantidad;
+       
+             total = producto.precio * producto.cantidad;
     })
-    
+
+    console.log(total)
 }
 
+//HASTA AQUI SUMA PERFECTO//
 
 const eliminarProductoDelCarrito = (id)=> {
 
-    console.log(carrito[id].cantidad); //1
-    carrito[id].cantidad--;
-    console.log(carrito[id].cantidad); 
+    console.log(carrito[BBDD].cantidad); //1
+    carrito[BBDD].cantidad--;
 
-    if(carrito[id].cantidad == 0){
-        
-        carrito.splice(id, 1);
+    console.log(carrito[BBDD].cantidad); 
+
+    if(carrito[BBDD].cantidad == 0){
+        carrito.reverse(cantidad, 1);}
+               
+     renderizarCarrito();
+
     }
     
-    renderizarCarrito();
-}
-
 
 function filtroPrecio(){
 
@@ -357,4 +129,5 @@ function filtroPrecio(){
     console.log(bd);
 
 }
+
 
